802fb67811ede2551738f857161ee19a5820d93a
Ian Del Duca <ianmdelduca@gmail.com>
2019-12-05 16:21:26 -0500

    refactor invalid input test into several tests with more descriptive titles for easier debugging

--- spec/twelveDaysOfChristmas.test.js
+++ spec/twelveDaysOfChristmas.test.js
@@ -5,17 +5,40 @@ describe("twelveDaysOfChristmas/1", () => {
   // returns the lyrics *up until and for* that day
   //   - lyrics available at https://www.41051.com/xmaslyrics/twelvedays.html
   describe("Invalid Inputs", () => {
+    const error = "Day must be an integer between 1 and 12";
 
     it("returns an error if given an in put that is not an integer between 1 - 12", () => {
-      const error = "Day must be an integer between 1 and 12";
-
       const result1 = twelveDaysOfChristmas.song(0);
-      const result2 = twelveDaysOfChristmas.song("merry xmas");
-      const result3 = twelveDaysOfChristmas.song(null);
-
       expect(result1).toEqual(error);
-      expect(result2).toEqual(error);
-      expect(result3).toEqual(error);
     });
+    it('returns an error if input is string', () => {
+      const result2 = twelveDaysOfChristmas.song("merry xmas");
+      expect(result2).toEqual(error);
+
+    })
+    it('returns an error if input is null', () => {
+      const result3 = twelveDaysOfChristmas.song(null);
+      expect(result3).toEqual(error);
+
+    })
+    it('returns an error if input is a negative number', () => {
+      const result4 = twelveDaysOfChristmas.song(-1);
+      expect(result4).toEqual(error);
+
+    })
+    it('returns an error if input is larger than 12', () => {
+      const result5 = twelveDaysOfChristmas.song(13);
+      expect(result5).toEqual(error);
+
+    })
+    it('returns an error if input is a float', () => {
+      const result6 = twelveDaysOfChristmas.song(3.14);
+      expect(result6).toEqual(error);
+
+    })
+    it('returns an error if input is an empty string', () => {
+      const result7 = twelveDaysOfChristmas.song("");
+      expect(result7).toEqual(error);
+    })
   })
 })
