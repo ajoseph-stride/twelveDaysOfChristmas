2caaa69510dc4f1829d01a0c49799daa6f995eb0
Anurup Joseph <ajoseph@stridenyc.com>
2019-12-05 15:03:22 -0500

    Updating test description.

--- spec/twelveDaysOfChristmas.test.js
+++ spec/twelveDaysOfChristmas.test.js
@@ -4,7 +4,7 @@ describe("twelveDaysOfChristmas/1", () => {
   // test-drive a function that, given a number representing the day of christmas,
   // returns the lyrics *up until and for* that day
   //   - lyrics available at https://www.41051.com/xmaslyrics/twelvedays.html
-  it("given input less than one, expect error", () => {
+  it("return an error if given input less than one", () => {
       const error = "Must be larger than zero.";
       const result = twelveDaysOfChristmas.song(0);
       expect(result).toEqual(error);
